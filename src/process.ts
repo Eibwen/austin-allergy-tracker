8744231ac969fe878cdfc6c15c45c1e9b025a5a2
Eibwen <Eibwen@users.noreply.github.com>
2021-02-13 15:08:54 -0600

    Fix spelling mistake

--- src/process.ts
+++ src/process.ts
@@ -17,19 +17,19 @@ async function processLineByLine(filename) {
   const ignorePattern = new RegExp(/^(\W*|\];\W*)$/);
   const dataPattern = new RegExp(/^{ y: new Date\(\'(.+?)\'\).toLocaleDateString\(\'en-US\', options\), a: (\d+) },?\W?$/);
 
-  const allerginData = {};
+  const allergenData = {};
 
-  let allerginType = '';
+  let allergenType = '';
   for await (const line of rl) {
     if (line.startsWith('var ary')) {
       // Old data
-      if (allerginType) {
-        console.log(`INFO: For: ${allerginType} got ${Object.keys(allerginData[allerginType]).length} records`);
+      if (allergenType) {
+        console.log(`INFO: For: ${allergenType} got ${Object.keys(allergenData[allergenType]).length} records`);
       }
 
       // New data
-      allerginType = line.match(/var ary(.+)\b/)[1];
-      // console.log(`INFO: allergin type set: ${allerginType}`);
+      allergenType = line.match(/var ary(.+)\b/)[1];
+      // console.log(`INFO: allergen type set: ${allergenType}`);
       continue;
     }
 
@@ -41,19 +41,19 @@ async function processLineByLine(filename) {
     let dataLine = dataPattern.exec(line);
     if (dataLine != null) {
       console.log(`DEBUG: add data`)
-      allerginData[allerginType] = allerginData[allerginType] || {};
-      allerginData[allerginType][dataLine[1]] = dataLine[2];
+      allergenData[allergenType] = allergenData[allergenType] || {};
+      allergenData[allergenType][dataLine[1]] = dataLine[2];
     }
     else {
       console.log(`WARNING: Unmatched line: '${line}'`)
     }
   }
 
-  for (const allerginProp in allerginData)
+  for (const allergenProp in allergenData)
   {
-    console.log(`INFO: Writing ${allerginProp} data to file`)
-    const filename = `alergin.${allerginProp}.json`;
-    const newData = allerginData[allerginProp];
+    console.log(`INFO: Writing ${allergenProp} data to file`)
+    const filename = `alergin.${allergenProp}.json`;
+    const newData = allergenData[allergenProp];
 
     fs.readFile(filename, (err, data) => {
       // TODO consider parsing this as a Map?
@@ -71,14 +71,14 @@ async function processLineByLine(filename) {
 
 
   console.log('INFO: updating README.md')
-  updateReadme(allerginData);
+  updateReadme(allergenData);
 }
 
 
 function wrapWithInjectionSpot(label, str) {
   return `<!-- ${label} -->${str}<!-- END ${label} -->`;
 }
-function updateReadme(allerginData) {
+function updateReadme(allergenData) {
   const filename = 'README.md';
   const injectionLabel = 'INJECT FORECAST';
 
@@ -89,9 +89,9 @@ function updateReadme(allerginData) {
     }
 
     let allergyRenderLines = [''];
-    for (const allerginProp in allerginData)
+    for (const allergenProp in allergenData)
     {
-      const data = allerginData[allerginProp];
+      const data = allergenData[allergenProp];
       const keys = Object.keys(data);
       const newestDateLabel = keys[keys.length - 1];
       const newestAllergenValue = data[newestDateLabel];
@@ -100,7 +100,7 @@ function updateReadme(allerginData) {
       const monthData = keys.slice(-30).map(x => data[x]);
       const weekData = keys.slice(-7).map(x => data[x]);
 
-      allergyRenderLines.push(`- **${allerginProp}: ${newestAllergenValue}** (${newestDateLabel})  [week high: ${Math.max(...weekData)}, month: ${Math.max(...monthData)}]`);
+      allergyRenderLines.push(`- **${allergenProp}: ${newestAllergenValue}** (${newestDateLabel})  [week high: ${Math.max(...weekData)}, month: ${Math.max(...monthData)}]`);
     }
     allergyRenderLines.push('');
 
